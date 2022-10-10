d7f277c6bcac3ee0ff6c5e4d889562c272ec13e6
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-10-10 22:22:31 +0700

    define migration configuration for initialize user table

---
+++ database/migrations/2022_10_10_050149_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->string('email', 64);
+            $table->string('phone_number', 16);
+            $table->string('password');
+            $table->string('avatar');
+            $table->boolean('is_admin');
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

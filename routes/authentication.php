5925c573537b490c1484434215bba7f9bf46eed0
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-10-10 22:25:14 +0700

    define router for API of signup

---
+++ routes/authentication.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->group(['prefix' => 'api/v1/auth'], function () use ($router) {
+    $router->post('signup-customer', 'AuthController@SignUp');
+    $router->post('admin/signup', 'AuthController@SignUp');
+});

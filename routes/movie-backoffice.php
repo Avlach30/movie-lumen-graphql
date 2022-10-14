c85bf7fe92224461b5d01bce09b13d45b4ff2405
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-10-14 16:39:04 +0700

    define router for API of create new studio

--- routes/movie-backoffice.php
+++ routes/movie-backoffice.php
@@ -6,4 +6,6 @@
 $router->group(['prefix' => 'api/v1/backoffice',  'middleware' => 'auth'], function () use ($router) {
     $router->post('movies', 'MovieController@CreateNewMovieWithTags');
     $router->get('movies', 'MovieController@GetAllMovieWithTags');
+
+    $router->post('studios', 'StudioController@CreateNewStudio');
 });

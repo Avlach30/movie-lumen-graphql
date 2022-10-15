a932506f1681a47cb7331d0bc2b1ffc87f02ac45
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-10-15 19:17:29 +0700

    registering repository providers in main file of lumen app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -94,6 +94,7 @@ $app->routeMiddleware([
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\RepositoryServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);

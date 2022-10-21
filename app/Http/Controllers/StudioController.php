bc3b6325afd0d12ea5d6b180cf7e3bac3fd8c757
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-10-21 10:13:25 +0700

    fixing validation error response in create new movie studio

--- app/Http/Controllers/StudioController.php
+++ app/Http/Controllers/StudioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Studio;
 use App\Helper\ApiResponse;
@@ -25,10 +26,22 @@ class StudioController extends Controller
         $studioNumber = $request->input('studio_number');
         $seatCapacity = $request->input('seat_capacity');
 
-        $this->validate($request, [
+        $validationRules = [
             'studio_number' => 'required|integer|gt:0',
             'seat_capacity' => 'required|integer|gt:25',
-        ]);
+        ];
+        $validationErrorMessage = [
+            'studio_number.required' => 'studio_number input field must be required',
+            'studio_number.integer' => 'studio_number input field must be integer format',
+            'studio_number.gt' => 'studio_number input field value must be greater than 0',
+            'seat_capacity.required' => 'seat_capacity input field must be required',
+            'seat_capacity.integer' => 'seat_capacity input field must be integer format',
+            'studio_number.gt' => 'studio_number input field value must be greater than 25',
+        ];
+        $validation = Validator::make($request->all(), $validationRules, $validationErrorMessage);
+        if ($validation->fails()) {
+            return $this->errorResponse($validation->errors(), 422);
+        }
 
         $isExist = $this->studioInterface->FindStudioByNumber($studioNumber);
         if ($isExist != null) {
